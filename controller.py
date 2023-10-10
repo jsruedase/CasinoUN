592cd11b95041070f9548866fbdc33cf4adfe052
Jsrueda <js.ruedas1@uniandes.edu.co>
2023-10-10 09:58:37 -0500

    base datos

--- controller.py
+++ controller.py
@@ -1,1 +1,8 @@
+import config as cf
+import model
+import time
+import csv
+csv.field_size_limit(2147483647)
 
+file = cf.data_dir + "base_datos.csv"
+input_file = list(csv.DictReader(open(file, encoding="utf-8")))
