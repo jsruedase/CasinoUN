20d6bffbc1cbd3824980d965479f3e6a95d58faf
Jsrueda <js.ruedas1@uniandes.edu.co>
2023-10-11 12:03:33 -0500

    cambios

--- controller.py
+++ controller.py
@@ -1,8 +1,26 @@
 import config as cf
 import model
 import time
+import datetime
 import csv
 csv.field_size_limit(2147483647)
 
-file = cf.data_dir + "base_datos.csv"
+file = cf.file_dir + "base_datos.csv"
 input_file = list(csv.DictReader(open(file, encoding="utf-8")))
+print(input_file)
+
+def inicio_usuario(nombre, cedula):
+    fecha = datetime.date.today()
+    lista_cedulas = []
+    for jugador in input_file:
+        lista_cedulas.append(jugador["Cedula"])
+        
+    if cedula not in lista_cedulas:
+        meter_jugador(nombre, cedula, fecha)
+            
+def meter_jugador(nombre, cedula, fecha):
+    editor = csv.writer(open(file, "a", encoding="utf-8"), delimiter=",")
+    editor.writerow([nombre, cedula, fecha, 500, 0, 0, 0, 0, 0])
+
+inicio_usuario("Juan", "1014861796")
+#inicio_usuario("Juan", "1014861796")
