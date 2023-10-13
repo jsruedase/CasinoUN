aaacd92af1439c9bfb11f0edd240593c21069ef0
Jsrueda <js.ruedas1@uniandes.edu.co>
2023-10-13 18:21:47 -0500

    Actualización view

--- controller.py
+++ controller.py
@@ -7,7 +7,7 @@ csv.field_size_limit(2147483647)
 
 file = cf.file_dir + "base_datos.csv"
 input_file = list(csv.DictReader(open(file, encoding="utf-8")))
-print(input_file)
+#print(input_file)
 
 def inicio_usuario(nombre, cedula):
     fecha = datetime.date.today()
@@ -17,10 +17,14 @@ def inicio_usuario(nombre, cedula):
         
     if cedula not in lista_cedulas:
         meter_jugador(nombre, cedula, fecha)
-            
+        return "Usuario añadido correctamente"
+    else:
+        return f"Usuario encontrado correctamente, bienvenido de vuelta, {nombre}"
+    
+    
 def meter_jugador(nombre, cedula, fecha):
     editor = csv.writer(open(file, "a", encoding="utf-8"), delimiter=",")
     editor.writerow([nombre, cedula, fecha, 500, 0, 0, 0, 0, 0])
 
-inicio_usuario("Juan", "1014861796")
+#inicio_usuario("Juan", "1014861797")
 #inicio_usuario("Juan", "1014861796")
--- view.py
+++ view.py
@@ -1,1 +1,48 @@
+import controller
+from tkinter import *               #Importa la interfaz grafica
+from tkinter import messagebox
+from PIL import ImageTk, Image      #Importa una libreria para abrir imágenes   
 
+# Creación de la interfaz gráfica usando Tkinter
+raiz = Tk() 
+raiz.title("Casino")
+raiz.geometry("1280x720")
+frame = Frame(raiz, width=600, height=400)
+frame.pack()
+frame.place(anchor='center', relx=0.5, rely=0.5)
+img = ImageTk.PhotoImage(Image.open("fondo-casino.jpg"))
+label = Label(frame, image = img)
+label.pack()
+titulo = Label(raiz, text="CASINO UN", bg="dark blue", font="Inkfree 20 bold")
+titulo.place(x = 550, y = 0)
+#Inicializa la ventana y crea un frame, el fondo
+
+nombre_e = Entry(raiz, width=20)
+nombre_e.place(x = 550, y = 50)
+nombre_e.insert(0, "Ingrese su nombre: ")
+
+cedula_e = Entry(raiz, width=20)
+cedula_e.place(x = 550, y = 90)
+cedula_e.insert(1, "Ingrese su cédula: ")
+
+edad_e =Entry(raiz, width=20)
+edad_e.place(x = 550, y = 130)
+edad_e.insert(2, "Ingrese su edad: ")
+
+#Se piden datos al usuario y se recopilan
+
+def click():
+    if nombre_e.get() == "" or cedula_e.get() == "" or edad_e.get() == "":
+        messagebox.showwarning("ERROR", "Por favor ingrese todos los datos correctamente")
+    elif int(edad_e.get()) < 18:
+        messagebox.showwarning("ERROR", "Debe ser mayor de edad para acceder")
+    else:
+        texto = controller.inicio_usuario(nombre_e.get(), cedula_e.get())
+        print(texto)
+        messagebox.showinfo("SUCCESS", texto)
+     
+boton_iniciar_sesion = Button(raiz, text="Iniciar sesión", command=click)
+boton_iniciar_sesion.place(x = 550, y = 170)
+
+
+raiz.mainloop()
