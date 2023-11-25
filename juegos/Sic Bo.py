94aed1a6473f0c37d6f0d43e01fecfb370daaf6f
NewForest2005 <149899338+NewForest2005@users.noreply.github.com>
2023-11-24 22:55:47 -0500

    Update Sic Bo.py

--- juegos/Sic Bo.py
+++ juegos/Sic Bo.py
@@ -1,74 +1,213 @@
-import random
 import tkinter as tk
 from tkinter import messagebox
+import pandas as pd
+import random
+from tkinter import ttk  # Agregada importación para el combobox
+from juegos import spaceman
 
-class SicBoGame:
-    def __init__(self, master, num_players):
-        self.master = master
-        self.master.title("Sic Bo Game")
-        self.num_players = num_players
-        self.jugadores = {f"Jugador {i+1}": 0 for i in range(num_players)}
-        
-        self.dados_label = tk.Label(master, text="¡Bienvenido a Sic Bo!")
-        self.dados_label.pack()
+# Cargar la base de datos
+base_datos = pd.read_csv('base_datos.csv')
 
-        self.apuesta_label = tk.Label(master, text="Ingresa tu apuesta:")
-        self.apuesta_label.pack()
+# Crear el diccionario de jugadores
+jugadores = {cedula: {'Nombre': nombre, 'Dinero_inicial': dinero_inicial, 'Dinero_juego2': 0} for _, nombre, cedula, _, dinero_inicial, _, _, _, _, _, _ in base_datos.itertuples(index=False)}
 
-        self.apuesta_entry = tk.Entry(master)
-        self.apuesta_entry.pack()
+# Crear listas para cédulas y nombres ordenadas por cédula
+cedulas_ordenadas = sorted(jugadores.keys())
+nombres_ordenados = [jugadores[cedula]['Nombre'] for cedula in cedulas_ordenadas]
 
-        self.apostar_button = tk.Button(master, text="Apostar", command=self.realizar_apuesta)
-        self.apostar_button.pack()
+num_jugador_actual = 0
+rondas_totales = 10
+ronda_actual = 1
 
-        self.tablero_label = tk.Label(master, text="----- Sic Bo -----")
-        self.tablero_label.pack()
+def registrar_jugadores():
+    global num_jugador_actual, num_jugadores_registrados, jugadores_registrados
+    try:
+        num_jugadores_registrados = int(num_players_entry.get())
+        jugadores_registrados = []
+        registrar_button.config(state=tk.NORMAL)  # Habilitar el botón de registrar jugador
+        registrar_jugador()
+    except ValueError:
+        messagebox.showerror("Error", "Por favor, ingresa un número válido para el número de jugadores.")
 
-        self.tablero_text = tk.Text(master, height=5, width=40)
-        self.tablero_text.pack()
+def registrar_jugador():
+    global num_jugador_actual, num_jugadores_registrados, jugadores_registrados
+    if num_jugador_actual < num_jugadores_registrados:
+        cedula = cedula_entry.get().strip()
+        if cedula in jugadores and cedula not in jugadores_registrados:
+            jugadores_registrados.append(cedula)
+            num_jugador_actual += 1
+            cedula_entry.delete(0, tk.END)
+            if num_jugador_actual == num_jugadores_registrados:
+                iniciar_juego()
+            else:
+                registrar_jugador()
+        else:
+            messagebox.showerror("Error", "La cédula no está registrada o ya se ha registrado. Inténtalo de nuevo.")
+    else:
+        messagebox.showinfo("Información", "Todos los jugadores han sido registrados.")
+        registrar_button.config(state=tk.DISABLED)  # Inhabilitar el botón de registrar jugador cuando todos están registrados
 
-        self.jugar_nuevamente_button = tk.Button(master, text="Jugar Nuevamente", command=self.jugar_nuevamente)
-        self.jugar_nuevamente_button.pack()
 
-        self.salir_button = tk.Button(master, text="Salir", command=self.salir)
-        self.salir_button.pack()
+def iniciar_juego():
+    global num_jugador_actual
+    actualizar_labels()
+    apostar_button.config(state=tk.NORMAL)
 
-    def lanzar_dados(self):
-        self.dados = [random.randint(1, 6) for _ in range(3)]
-        resultado = sum(self.dados)
-        self.dados_label.config(text=f"¡Resultado de los dados: {self.dados[0]}, {self.dados[1]}, {self.dados[2]}!")
-        return resultado
+def lanzar_dados():
+    dados = [random.randint(1, 6) for _ in range(3)]
+    resultado = sum(dados)
+    dados_label.config(text=f"¡Resultado de los dados: {dados[0]}, {dados[1]}, {dados[2]}!")
+    return resultado
 
-    def realizar_apuesta(self):
-        try:
-            apuesta = int(self.apuesta_entry.get())
+def realizar_apuesta():
+    global num_jugador_actual, ronda_actual
+    try:
+        apuesta_dinero = int(apuesta_entry_dinero.get().strip())
+        if 40000 <= apuesta_dinero <= 160000:
+            jugadores[jugadores_registrados[num_jugador_actual]]['Dinero_inicial'] -= apuesta_dinero
+            jugadores[jugadores_registrados[num_jugador_actual]]['Dinero_juego2'] += apuesta_dinero
+            apuesta = int(apuesta_entry.get().strip())
             if 4 <= apuesta <= 18:
-                resultado = self.lanzar_dados()
-                self.verificar_apuestas(apuesta, resultado)
+                jugadores[jugadores_registrados[num_jugador_actual]]['Apuesta'] = apuesta
+                num_jugador_actual += 1
+
+                if num_jugador_actual == len(jugadores_registrados):
+                    resultado = lanzar_dados()
+                    verificar_apuestas(resultado)
+                    num_jugador_actual = 0
+                    ronda_actual += 1
+                    if ronda_actual > rondas_totales:
+                        finalizar_juego()
+                    else:
+                        actualizar_labels()
+                else:
+                    apuesta_entry.delete(0, tk.END)
+                    apuesta_label.config(text=f"Apuesta {jugadores_registrados[num_jugador_actual]} - Ronda {ronda_actual}:")
             else:
                 messagebox.showerror("Error", "La apuesta debe estar entre 4 y 18.")
-        except ValueError:
-            messagebox.showerror("Error", "Por favor, ingresa un número válido.")
+        else:
+            messagebox.showerror("Error", "La apuesta de dinero debe estar entre 40000 y 160000.")
+    except ValueError:
+        messagebox.showerror("Error", "Por favor, ingresa números válidos.")
 
-    def verificar_apuestas(self, apuesta, resultado):
-        self.tablero_text.delete(1.0, tk.END)
-        for jugador in self.jugadores:
-            if apuesta == resultado:
-                self.jugadores[jugador] += 1
-            self.tablero_text.insert(tk.END, f"{jugador}: {self.jugadores[jugador]} puntos\n")
-        self.apuesta_entry.delete(0, tk.END)
+def verificar_apuestas(resultado):
+    tablero_text.delete(1.0, tk.END)
+    ganadores = [cedula for cedula in jugadores_registrados if jugadores[cedula]['Apuesta'] == resultado]
+    total_apuesta = sum(jugadores[cedula]['Apuesta'] for cedula in jugadores_registrados)
 
-    def jugar_nuevamente(self):
-        self.dados_label.config(text="¡Bienvenido a Sic Bo!")
-        self.tablero_text.delete(1.0, tk.END)
+    if ganadores:
+        monto_por_ganador = total_apuesta / len(ganadores)
+        for ganador in ganadores:
+            jugadores[ganador]['Dinero_inicial'] += monto_por_ganador
+            jugadores[ganador]['Dinero_juego2'] -= monto_por_ganador
+            tablero_text.insert(tk.END, f"{jugadores[ganador]['Nombre']} gana {monto_por_ganador} puntos en la ronda {ronda_actual}!\n")
+    else:
+        tablero_text.insert(tk.END, f"Nadie ha acertado en esta ronda, no hay ganadores.\n")
 
-    def salir(self):
-        self.master.destroy()
+    mostrar_puntuaciones()
 
-def main():
-    root = tk.Tk()
-    num_players = int(input("Ingrese el número de jugadores: "))
-    juego = SicBoGame(root, num_players)
-    root.mainloop()
+def mostrar_puntuaciones():
+    tablero_text.insert(tk.END, "\nTabla de Puntuaciones:\n")
+    for cedula in jugadores_registrados:
+        tablero_text.insert(tk.END, f"{jugadores[cedula]['Nombre']} - Dinero Inicial: {jugadores[cedula]['Dinero_inicial']} - Dinero Juego 2: {jugadores[cedula]['Dinero_juego2']}\n")
 
-if __name__ == "__main__":
+def finalizar_juego():
+    tablero_text.insert(tk.END, "\nJuego finalizado. Puntuaciones finales:\n")
+    mostrar_puntuaciones()
+    apostar_button["state"] = tk.DISABLED
+
+def jugar_nuevamente():
+    global ronda_actual, num_jugador_actual
+    ronda_actual = 1
+    num_jugador_actual = 0
+    jugadores.clear()
+    actualizar_labels()
+    num_players_entry.delete(0, tk.END)
+    apuesta_entry_dinero.delete(0, tk.END)
+    apuesta_entry.delete(0, tk.END)
+    tablero_text.delete(1.0, tk.END)
+    iniciar_registro()
+
+def salir():
+    # Actualizar la base de datos antes de salir
+    for cedula in jugadores:
+        base_datos.loc[base_datos['Cedula'] == int(cedula), 'Dinero_inicial'] = jugadores[cedula]['Dinero_inicial']
+        base_datos.loc[base_datos['Cedula'] == int(cedula), 'Dinero_juego2'] = jugadores[cedula]['Dinero_juego2']
+    base_datos.to_csv('base_datos.csv', index=False)
+    root.destroy()
+
+def actualizar_labels():
+    apuesta_label.config(text=f"Apuesta {jugadores_registrados[num_jugador_actual]} - Ronda {ronda_actual}:")
+
+def iniciar_registro():
+    global num_jugador_actual
+    num_jugador_actual = 0
+    num_players_entry.config(state=tk.NORMAL)
+    cedula_entry.config(state=tk.NORMAL)
+    registrar_button.config(state=tk.NORMAL)
+    registrar_button.pack()
+
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Sic Bo Game")
+root.geometry("400x400")
+
+# Etiquetas
+num_players_label = tk.Label(root, text="Ingrese el número de jugadores:")
+num_players_label.pack()
+
+num_players_entry = tk.Entry(root)
+num_players_entry.pack()
+
+# Registro de jugadores
+cedula_label = tk.Label(root, text="Ingrese su cédula:")
+cedula_label.pack()
+
+cedula_entry = tk.Entry(root)
+cedula_entry.pack()
+
+registrar_button = tk.Button(root, text="Registrar Jugador", command=registrar_jugador, state=tk.DISABLED)
+registrar_button.pack()
+
+# Botones adicionales
+iniciar_registro_button = tk.Button(root, text="Iniciar Registro", command=iniciar_registro)
+iniciar_registro_button.pack()
+
+iniciar_juego_button = tk.Button(root, text="Iniciar Juego", command=registrar_jugadores, state=tk.DISABLED)
+iniciar_juego_button.pack()
+
+dados_label = tk.Label(root, text="¡Bienvenido a Sic Bo!")
+dados_label.pack()
+
+apuesta_label_dinero = tk.Label(root, text="Ingrese su apuesta de dinero (40000-160000):")
+apuesta_label_dinero.pack()
+
+apuesta_entry_dinero = tk.Entry(root)
+apuesta_entry_dinero.pack()
+
+apuesta_label = tk.Label(root, text="")
+apuesta_label.pack()
+
+apuesta_entry = tk.Entry(root)
+apuesta_entry.pack()
+
+apostar_button = tk.Button(root, text="Apostar", command=realizar_apuesta, state=tk.DISABLED)
+apostar_button.pack()
+
+# Área de texto
+tablero_text = tk.Text(root, height=5, width=40)
+tablero_text.pack()
+
+# Botones adicionales
+jugar_nuevamente_button = tk.Button(root, text="Jugar Nuevamente", command=jugar_nuevamente)
+jugar_nuevamente_button.pack()
+
+# Botón para jugar Sic Bo
+jugar_sic_bo_button = tk.Button(root, text="Jugar Sic Bo", command=spaceman)  # Añadido el botón para jugar Sic Bo
+jugar_sic_bo_button.pack()
+
+salir_button = tk.Button(root, text="Salir", command=salir)
+salir_button.pack()
+
+# Iniciar el bucle principal
+root.mainloop()
