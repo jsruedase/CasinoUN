fe8f00b5821cd4bef43defef6fb91e0c18d8cae9
NewForest2005 <149899338+NewForest2005@users.noreply.github.com>
2023-11-27 10:11:23 -0500

    Update black_jack.py

--- juegos/black_jack.py
+++ juegos/black_jack.py
@@ -16,7 +16,6 @@ cartas_dealer = []
 cartas_cpu1 = []
 cartas_cpu2 = []
 cartas_cpu3 = []
-jugadores_plantados = []
 
 cartas_ganadas = {'Jugador': 0, 'Dealer': 0, 'CPU1': 0, 'CPU2': 0, 'CPU3': 0}
 rondas_ganadas = {'Jugador': 0, 'Dealer': 0, 'CPU1': 0, 'CPU2': 0, 'CPU3': 0}
@@ -85,23 +84,19 @@ tabla_cartas_ganadas.pack(side=tk.RIGHT, padx=10, pady=10)
 
 def nueva_partida():
     global baraja, cartas_jugador, cartas_dealer, cartas_cpu1, cartas_cpu2, cartas_cpu3, cartas_ganadas, rondas_ganadas, numero_ronda
-    if numero_ronda == 30:
-        messagebox.showinfo("Fin de juego", "Se han completado 30 rondas. Juego terminado.")
-        root.quit()
-    else:
-        baraja = baraja_inicial.copy()
-        cartas_jugador = []
-        cartas_dealer = []
-        cartas_cpu1 = []
-        cartas_cpu2 = []
-        cartas_cpu3 = []
-        cartas_ganadas = {'Jugador': 0, 'Dealer': 0, 'CPU1': 0, 'CPU2': 0, 'CPU3': 0}
-        rondas_ganadas = {'Jugador': 0, 'Dealer': 0, 'CPU1': 0, 'CPU2': 0, 'CPU3': 0}
-        numero_ronda += 1
-        habilitar_botones()
-        repartir_cartas_iniciales()
-        ocultar_cartas_cpu()  # Nueva línea para ocultar las cartas de los CPU
-        actualizar_labels()
+    baraja = baraja_inicial.copy()
+    cartas_jugador = []
+    cartas_dealer = []
+    cartas_cpu1 = []
+    cartas_cpu2 = []
+    cartas_cpu3 = []
+    cartas_ganadas = {'Jugador': 0, 'Dealer': 0, 'CPU1': 0, 'CPU2': 0, 'CPU3': 0}
+    rondas_ganadas = {'Jugador': 0, 'Dealer': 0, 'CPU1': 0, 'CPU2': 0, 'CPU3': 0}
+    numero_ronda += 1
+    habilitar_botones()
+    repartir_cartas_iniciales()
+    ocultar_cartas_cpu()  # Nueva línea para ocultar las cartas de los CPU
+    actualizar_labels()
 
 def habilitar_botones():
     button_pedir["state"] = "active"
@@ -162,10 +157,7 @@ def pedir_carta():
             determinar_ganador_21()
         else:
             jugar_cpu()
-          
-def mostrar_perdedor(perdedor):
-  messagebox.showinfo("Perdedor", f"{perdedor} ha perdido. ¡Se pasó de 21!")
-  
+
 def plantarse():
     while sumar_cartas(cartas_dealer) < 17:
         cartas_dealer.append(extraer_carta())
@@ -178,11 +170,7 @@ def plantarse():
 
     actualizar_labels()
 
-    determinar_ganador(sumar_cartas(cartas_jugador), "Jugador")
-    determinar_ganador(sumar_cartas(cartas_dealer), "Dealer")
-    determinar_ganador(sumar_cartas(cartas_cpu1), "CPU 1")
-    determinar_ganador(sumar_cartas(cartas_cpu2), "CPU 2")
-    determinar_ganador(sumar_cartas(cartas_cpu3), "CPU 3")
+    determinar_ganador_ronda()
 
 def jugar_cpu():
     while sumar_cartas(cartas_cpu1) < 17:
@@ -194,9 +182,7 @@ def jugar_cpu():
 
     actualizar_labels()
 
-    determinar_ganador(sumar_cartas(cartas_cpu1), "CPU 1")
-    determinar_ganador(sumar_cartas(cartas_cpu2), "CPU 2")
-    determinar_ganador(sumar_cartas(cartas_cpu3), "CPU 3")
+    determinar_ganador_ronda()
 
 def extraer_carta():
     carta = random.choice(baraja)
@@ -249,69 +235,8 @@ def ocultar_cartas_cpu():
     cartas_cpu2 = ["?"] * 2
     cartas_cpu3 = ["?"] * 2
 
-def plantarse():
-  global jugadores_plantados
-  if not jugadores_plantados:
-      jugadores_plantados.append("Jugador")
-      while sumar_cartas(cartas_dealer) < 17:
-          cartas_dealer.append(extraer_carta())
-      while sumar_cartas(cartas_cpu1) < 17:
-          cartas_cpu1.append(extraer_carta())
-      while sumar_cartas(cartas_cpu2) < 17:
-          cartas_cpu2.append(extraer_carta())
-      while sumar_cartas(cartas_cpu3) < 17:
-          cartas_cpu3.append(extraer_carta())
-
-      actualizar_labels()
-
-      determinar_ganador_ronda()
-
-def jugar_cpu_auto(cartas_cpu, label_cartas_cpu):
-  while sumar_cartas(cartas_cpu) < 17:
-      cartas_cpu.append(extraer_carta())
-      actualizar_labels()
-      root.update_idletasks()  # Actualizar la interfaz gráfica durante el bucle para mostrar los movimientos
-      root.after(1000)  # Esperar 1 segundo entre cada movimiento de CPU (ajustable según sea necesario)
-
-
-def jugar_round_auto():
-  global jugadores_plantados
-  if not jugadores_plantados:
-      # Jugador
-      jugadores_plantados.append("Jugador")
-      pedir_carta()
-      root.update_idletasks()
-      root.after(1000)
-
-      # Dealer
-      jugadores_plantados.append("Dealer")
-      jugar_cpu_auto(cartas_dealer, label_cartas_dealer)
-      root.update_idletasks()
-      root.after(1000)
-
-      # CPU 1
-      jugadores_plantados.append("CPU1")
-      jugar_cpu_auto(cartas_cpu1, label_cartas_cpu1)
-      root.update_idletasks()
-      root.after(1000)
-
-      # CPU 2
-      jugadores_plantados.append("CPU2")
-      jugar_cpu_auto(cartas_cpu2, label_cartas_cpu2)
-      root.update_idletasks()
-      root.after(1000)
-
-      # CPU 3
-      jugadores_plantados.append("CPU3")
-      jugar_cpu_auto(cartas_cpu3, label_cartas_cpu3)
-      root.update_idletasks()
-      root.after(1000)
-
-      determinar_ganador_ronda()
-
-
 def determinar_ganador_ronda():
-    global cartas_jugador, cartas_dealer, cartas_cpu1, cartas_cpu2, cartas_cpu3, cartas_ganadas, rondas_ganadas
+    global cartas_jugador, cartas_dealer, cartas_cpu1, cartas_cpu2, cartas_cpu3, cartas_ganadas, rondas_ganadas, numero_ronda
 
     puntajes = {
         'Jugador': sumar_cartas(cartas_jugador),
@@ -324,69 +249,46 @@ def determinar_ganador_ronda():
     max_puntaje = max(puntajes.values())
     ganadores = [jugador for jugador, puntaje in puntajes.items() if puntaje == max_puntaje]
 
-    if 'Jugador' in ganadores:
-        mostrar_resultado_ronda('Jugador', len(cartas_dealer) + len(cartas_cpu1) + len(cartas_cpu2) + len(cartas_cpu3))
-    elif ganadores:
-        mostrar_resultado_ronda(ganadores[0], len(cartas_dealer) // 2 + len(cartas_cpu1) // 2 + len(cartas_cpu2) // 2 + len(cartas_cpu3) // 2)
+    mensaje_ganadores = f"Ganador(es) de la ronda {numero_ronda}: {', '.join(ganadores)}"
+    messagebox.showinfo("Ganador de la ronda", mensaje_ganadores)
 
-def mostrar_ganadores(ganadores):
-    messagebox.showinfo("Ganadores", f"Los ganadores de la ronda son: {', '.join(ganadores)}")
+    for ganador in ganadores:
+        cartas_ganadas[ganador] += 1
+        rondas_ganadas[ganador] += 1
 
-def mostrar_resultado_ronda(ganador, cartas_repartidas):
-  global rondas_ganadas, cartas_ganadas
+    actualizar_tabla_cartas_ganadas()
+    actualizar_labels()
 
-  if ganador == 'Jugador':
-      messagebox.showinfo("Resultado de la Ronda", f"¡Has ganado la ronda! +1 punto")
-      rondas_ganadas['Jugador'] += 1
-  else:
-      messagebox.showinfo("Resultado de la Ronda", f"{ganador} ha ganado la ronda. +1 punto")
-
-  cartas_ganadas[ganador] += cartas_repartidas
-  actualizar_tabla_cartas_ganadas()
-
-  nueva_partida()  # Agregar esta línea para iniciar una nueva partida después de la ronda
-
-# Modificación de la función nueva_partida()
-def nueva_partida():
-  global baraja, cartas_jugador, cartas_dealer, cartas_cpu1, cartas_cpu2, cartas_cpu3, cartas_ganadas, rondas_ganadas, numero_ronda, jugadores_plantados
-  jugadores_plantados = []  # Restablecer la lista de jugadores plantados
-
-def determinar_ganador(puntaje, jugador):
-  global cartas_jugador, cartas_dealer, cartas_cpu1, cartas_cpu2, cartas_cpu3
-
-  if puntaje > 21:
-      mostrar_perdedor(jugador)
-  elif puntaje == 21:
-      mostrar_ganadores([jugador])
-  elif jugador == 'Jugador' and sumar_cartas(cartas_jugador) == 21:
-      mostrar_ganadores(['Jugador'])
-  elif jugador == 'Dealer' and sumar_cartas(cartas_dealer) == 21:
-      mostrar_ganadores(['Dealer'])
-  elif jugador == 'CPU1' and sumar_cartas(cartas_cpu1) == 21:
-      mostrar_ganadores(['CPU1'])
-  elif jugador == 'CPU2' and sumar_cartas(cartas_cpu2) == 21:
-      mostrar_ganadores(['CPU2'])
-  elif jugador == 'CPU3' and sumar_cartas(cartas_cpu3) == 21:
-      mostrar_ganadores(['CPU3'])
+    if numero_ronda == 30:
+        messagebox.showinfo("Fin de juego", "Se han completado 30 rondas. Juego terminado.")
+        root.quit()
+    else:
+        messagebox.showinfo("Nueva ronda", "Comienza una nueva ronda.")
+        nueva_partida()
 
 def actualizar_tabla_cartas_ganadas():
-  global cartas_ganadas, rondas_ganadas
-  tabla_cartas_ganadas.delete("1.0", tk.END)
-  for jugador in cartas_ganadas:
-      tabla_cartas_ganadas.insert(tk.END, f"{jugador}: Cartas Ganadas: {cartas_ganadas[jugador]}, Rondas Ganadas: {rondas_ganadas[jugador]}\n")
+    tabla_cartas_ganadas.delete(1.0, tk.END)
+    for jugador, cartas in cartas_ganadas.items():
+        tabla_cartas_ganadas.insert(tk.END, f"{jugador}: {cartas}\n")
 
-# Crear etiquetas y botones
-button_pedir = tk.Button(root, text="Pedir Carta", font=button_font, command=pedir_carta, bg="#b2f7e2", fg="#333333")  # Verde pastel
-button_pedir.pack(side=tk.LEFT, padx=10)
+# Crear botones
+button_pedir = tk.Button(root, text="Pedir Carta", font=button_font, command=pedir_carta, bg="#2ECC71")  # Verde
+button_pedir.pack(side=tk.LEFT, padx=10, pady=10)
 
-button_plantarse = tk.Button(root, text="Plantarse", font=button_font, command=plantarse, bg="#f7b2b2", fg="#333333")  # Rojo pastel
-button_plantarse.pack(side=tk.LEFT, padx=10)
+button_plantarse = tk.Button(root, text="Plantarse", font=button_font, command=plantarse, bg="#FF5733")  # Rojo menos brillante
+button_plantarse.pack(side=tk.LEFT, padx=10, pady=10)
 
-button_nueva_partida = tk.Button(root, text="Nueva Partida", font=button_font, command=nueva_partida, bg="#b2b2f7", fg="#333333")  # Azul pastel
-button_nueva_partida.pack(side=tk.LEFT, padx=10)
+button_nueva_partida = tk.Button(root, text="Nueva Partida", font=button_font, command=nueva_partida, bg="#3498DB")  # Azul
+button_nueva_partida.pack(side=tk.LEFT, padx=10, pady=10)
 
+# Inhabilitar el botón de nueva partida al inicio
+button_nueva_partida["state"] = "disabled"
 
-jugar_round_auto()
+# Iniciar el juego
+repartir_cartas_iniciales()
+ocultar_cartas_cpu()
+actualizar_labels()
 
-# Resto del código
+# Bucle principal de la interfaz gráfica
 root.mainloop()
+
