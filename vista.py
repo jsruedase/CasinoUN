483f85665de36e1a5eadcddb5699f2dbefb7b839
Jsrueda <js.ruedas1@uniandes.edu.co>
2023-11-28 09:32:06 -0500

    quitar listo

--- vista.py
+++ vista.py
@@ -103,6 +103,6 @@ boton_salir = Button(raiz, text="SALIR", command=salir, height=5, width=20, bg="
 boton_mostrar_stats = Button(raiz, text="Ver tus \nestadísticas", command=stats, height=5, width=20)
 boton_mostrar_stats_juego = Button(raiz, text="Ver el promedio\nde ganancia por juego", command=stats_juego, height=5, width=20)
 boton_salir.place(x = 100, y = 100)
-boton_listo.place(x = 550, y = 210) ##############################################
+#boton_listo.place(x = 550, y = 210) ##############################################
 
 raiz.mainloop() #Hace que la ventana siga abierta
